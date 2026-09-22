3e8cca133b23eeb9fcb5db05f13284486d494fdb
W1ther2 <ainius226@gmail.com>
2026-09-22 09:24:39 +0300

    Add files via upload

---
+++ tests/fixtures.py
@@ -0,0 +1,131 @@
+# -*- coding: utf-8 -*-
+"""Tikri HTML gabalai is skelbiu.lt (2026-09-21), kad parseris butu tikrinamas
+su tuo, ka svetaine is tikruju grazina, o ne su issigalvotu HTML."""
+
+# Saraso puslapio gabalas: trys skelbimai is /skelbimai/?keywords=iPhone+13&category_id=480&orderBy=1
+SKELBIU_LIST = """
+<div class="items-container">
+<a href="/skelbimai/iphone-13-pro-max-512gb-87196074.html"
+        class="js-cfuser-link standard-list-item   "
+    data-item-id="87196074"
+    id="ads87196074">
+            <div class="collapsed-info">
+            <div class="expand-icon js-collapse-button">
+                                    <div class="text">Rodyti</div>
+                            </div>
+            <div class="title">iPhone 13 pro max 512gb</div>
+            <div class="price-item"><div class="price">350 &euro;</div></div>
+            <div class="second-line">Panev&#279;&#382;ys, prie&#353; 7 min.</div>
+        </div>
+    <div class="extended-info">
+        <div class="img-block">
+            <div class="wrapper">
+                <div class="top-right"><!--
+                --><!--
+            --></div>
+                <div class="top-left">
+                                    </div>
+                <img src="https://skelbiu-img.dgn.lt/1_23_5182691844/iphone-13-pro-max-512gb.jpg" loading="lazy" draggable="false" fetchpriority="high"/>
+                            </div>
+        </div>
+        <div class="content-block">
+                            <button class="js-remember-button bookmark-button "
+                        data-login-uri="L3NrZWxiaW1haS8="></button>
+            <div class="title">iPhone 13 pro max 512gb</div>
+                            <div class="first-dataline">Geros b&#363;kles telefonas, visada naudotas su apsauginiu stiklu, <em>baterija</em>
+83%, komplekte d&#279;klas usb orginalus laidas ir de&#382;ut&#279;. </div>
+            <div class="second-dataline">Panev&#279;&#382;ys, prie&#353; 7 min.</div>
+            <div class="item-params">
+                <div class='param'>B&#363;kl&#279;: Naudota</div>                            </div>
+            <div class="price-line"><!--
+            --><div class="price">350 &euro;</div>                                                            </div>
+        </div>
+    </div>
+</a>
+</div>
+<div style="position: relative;">
+<a href="/skelbimai/13-pro-max-128-256gb-su-garantija-kaip-nauji-78523526.html"
+        class="js-cfuser-link standard-list-item bold"
+    data-item-id="78523526"
+    id="ads78523526">
+            <div class="collapsed-info">
+            <div class="title">13 pro max 128 -256gb su garantija, kaip Nauji!</div>
+            <div class="price-item"><div class="price">360 &euro;</div></div>
+            <div class="second-line">Vilnius, rugs&#279;jo 14 d.</div>
+        </div>
+    <div class="extended-info">
+        <div class="img-block"><div class="wrapper">
+                <img src="https://skelbiu-img.dgn.lt/1_23_4277811836/13-pro-max.jpg" loading="lazy"/>
+        </div></div>
+        <div class="content-block">
+            <div class="title">13 pro max 128 -256gb su garantija, kaip Nauji!</div>
+            <div class="first-dataline">Parduodami labai ma&#382;ai naudoti iphone 13 pro Max serijos telefonai i&#353; ekspozicijos...</div>
+            <div class="second-dataline">Vilnius, rugs&#279;jo 14 d.</div>
+            <div class="item-params">
+                <div class='param'>B&#363;kl&#279;: Naudota</div>                            </div>
+            <div class="price-line"><!--
+            --><div class="price">360 &euro;</div>   </div>
+        </div>
+    </div>
+</a>
+</div>
+<div style="position: relative;">
+<a href="/skelbimai/iphone-xiaomi-samsung-greitas-ekranu-keitimas-30555297.html"
+        class="js-cfuser-link standard-list-item   "
+    data-item-id="30555297"
+    id="ads30555297">
+            <div class="collapsed-info">
+            <div class="title">iPhone, Xiaomi, Samsung greitas ekran&#371; keitimas</div>
+            <div class="price-item"></div>
+            <div class="second-line">Vilnius, rugs&#279;jo 14 d.</div>
+        </div>
+    <div class="extended-info">
+        <div class="content-block">
+            <div class="title">iPhone, Xiaomi, Samsung greitas ekran&#371; keitimas</div>
+            <div class="first-dataline">80&euro; iPhone 12 / 12 Pro - 70&euro; <em>iPhone 13</em> mini - 80&euro; iPhone 13 Pro...</div>
+            <div class="second-dataline">Vilnius, rugs&#279;jo 14 d.</div>
+            <div class="item-params"> </div>
+            <div class="price-line"><!-- --> </div>
+        </div>
+    </div>
+</a>
+</div>
+"""
+
+# Skelbimo puslapis: privatus pardavejas, /skelbimai/iphone-13-87191092.html
+SKELBIU_ITEM = """<!DOCTYPE html><html><head>
+<meta property="og:title" content="iPhone 13"/>
+<meta property="og:description" content="Turi minimalius pabrai&#382;ymus, visada buvo naudotas su d&#279;klais, baterijos likutis 74% bet laiko dar gerai"/>
+<meta property="og:type" content="website"/>
+<meta property="og:url" content="https://www.skelbiu.lt/skelbimai/iphone-13-87191092.html"/>
+<meta property="og:image" content="https://skelbiu-img.dgn.lt/1_16_5182469856/iphone-13.jpg"/>
+</head><body>
+<div class="user-info" style="flex-wrap: wrap;">
+<div class="profile-image-wrapper"><div class="profile-image">
+<img src="https://static.skelbiu.lt/static/images/show_item_v1/user1.svg" alt="">
+<div class="icon-user-verified tooltip"></div>
+</div></div>
+<div class="profile-data-wrapper">
+<div class="profile-data-owner-name">
++3706231****                                                     </div>
+<div class="profile-info-line">
+U&#382;siregistravo 2024 gegu&#382;&#281;.
+<span>&#302;d&#279;jo skelbim&#371;: 1</span>
+</div>
+<a href="/pasiulymai/vardas/" class="all-sellers-items">Visi pardav&#279;jo skelbimai <span>3</span> </a>
+</div>
+</div>
+<div class="details-container item-card  ">
+<div class="details-grid">
+<div class="details-row"><label>Gamintojas:</label><span>Apple</span></div><div class="details-row"><label>Modelis:</label><span>iPhone 13</span></div><div class="details-row"><label>B&#363;kl&#279;:</label><span>Naudota</span></div>
+</div>
+</div>
+<p class="title">Apra&#353;ymas:</p>
+<div class="description">
+Turi minimalius pabrai&#382;ymus, visada buvo naudotas su d&#279;klais, baterijos likutis 74% bet laiko dar gerai, atiduodu d&#279;kliukus                    </div>
+<div class="price">200 &euro;</div>
+</body></html>"""
+
+# Istrintas / nebegaliojantis skelbimas
+SKELBIU_GONE = """<!DOCTYPE html><html><head><title>Puslapis nerastas - Skelbiu.lt</title></head>
+<body>Atsipra&#353;ome, ta&#269;iau tokio puslapio neradome.</body></html>"""
